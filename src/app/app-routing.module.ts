b8280867955f088f1ff96405c8c192b808bbfaa8
leo <leo.david.guerard@gmail.com>
2023-01-27 16:12:15 +0100

    add variables and card list and items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardViewComponent } from './card-view/card-view.component';
+import { ButtonsViewComponent } from './buttons-view/buttons-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: CardViewComponent },
+  { path: 'second-component', component: ButtonsViewComponent },
+  { path: '',   redirectTo: '/first-component', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
